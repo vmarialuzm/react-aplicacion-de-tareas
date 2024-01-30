5b48787aeb9b9fcdc52fc9b65587762f4b926af1
vmarialuz <vmarialuzm@gmail.com>
2024-01-30 18:41:07 -0500

    componente formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-import Tarea from './componentes/Tarea';
+import TareaFormulario from './componentes/Formulario';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       </div>
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
-        <Tarea texto='Aprender React'/>
+        <TareaFormulario />
       </div>
     </div>
   );
