d1fde468699aa2297e88780405c7e0f8cc3890fa
vmarialuz <vmarialuzm@gmail.com>
2024-01-30 18:14:34 -0500

    Lista de tareas

---
+++ src/componentes/Tarea.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../hojas-de-estilo/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Tarea({ texto }) {
+  return (
+    <div className='tarea-contenedor'>
+      <div className='tarea-texto'>
+        {texto}
+      </div>
+      <div className='tarea-contenedor-iconos'>
+        <AiOutlineCloseCircle className='tarea-icono'/>
+      </div>
+    </div>
+  )
+}
+
+export default Tarea;
