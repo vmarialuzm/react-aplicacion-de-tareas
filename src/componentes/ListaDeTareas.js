c41bfcb4ed3411cbe7e054b0d7de45327f8d5c54
vmarialuz <vmarialuzm@gmail.com>
2024-01-31 11:10:48 -0500

    Eliminar tarea

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -15,17 +15,34 @@ function ListaDeTareas() {
     }
   }
 
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
+  const completarTarea =id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  }
+
   return (
     <>
       <TareaFormulario onSubmit={agregarTarea} />
       <div className='tareas-lista-contenedor'>
         {
           tareas.map((tarea) => 
-          <Tarea 
-            key={tarea.id}
-            id={tarea.id}
-            texto={tarea.texto}
-            completada={tarea.completada}/>
+            <Tarea 
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada}
+              completarTarea={completarTarea}
+              eliminarTarea={eliminarTarea}/>
           )
         }
       </div>
